5f190196c9be7e6489b9ffe7149d0295228f513f
Vabs2 <vaibhavkadbhane2@gmail.com>
2020-10-23 10:01:36 +0530

    Flipping a coin coin multiple times and displaying the winner

--- Flip_Coin_Simulation_Problem.sh
+++ Flip_Coin_Simulation_Problem.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
-coin=0
 
-function head_or_tails()
-{
-        if [[ $((RANDOM%2)) -eq 1 ]]
-        then
-               echo coin=heads
-        else
-               echo coin=tails
-        fi
-}
-head_or_tails
+read -p "Enter Number : " number
+
+totalHeads=0
+totalTails=0
+
+if [[ $number ]] && [ $number -eq $number 2>/dev/null ] && [ $number -gt 0 ]; then
+	for (( i=0; i<$number; i++ ))
+	do
+
+		isCheck=$((RANDOM%2))
+
+		if [ "$isCheck" -eq 0 ]; then
+			totalHeads=$(($totalHeads + 1))
+		else
+			totalTails=$(($totalTails + 1))
+		fi
+	done
+
+	echo "Out Of $number : Heads Won $totalHeads times : Tails won $totalTails times"
+else
+	echo "Wrong Input !! Enter Number Above 0 Only. E.g  15 "
+fi
