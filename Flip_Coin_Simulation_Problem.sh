e8ecaa8a2b2d0ad749b6af9e6389ff1c329aef79
Vabs2 <vaibhavkadbhane2@gmail.com>
2020-10-23 10:08:57 +0530

    Extend UC3 and Continue the Game till the difference is 2

--- Flip_Coin_Simulation_Problem.sh
+++ Flip_Coin_Simulation_Problem.sh
@@ -1,31 +1,34 @@
-#! /bin/bash
+#! /bin/bash 
 
 totalHeads=0
 totalTails=0
 
-while [ "$totalHeads" -lt "21" -a "$totalTails" -lt "21" ]
+function flipCoins(){
+
+        isCheck=$((RANDOM%2))
+
+        if [ "$isCheck" -eq 0 ]; then
+                totalHeads=$(($totalHeads + 1))
+        else
+                totalTails=$(($totalTails + 1))
+        fi
+
+}
+
+while [ "$totalHeads" -lt 21 -a "$totalTails" -lt 21 -o $(("$totalHeads" - "$totalTails")) -lt 2 ]
 do
-
-	isCheck=$((RANDOM%2))
-
-	if [ "$isCheck" -eq 0 ]; then
-		totalHeads=$(($totalHeads + 1))
-	else
-		totalTails=$(($totalTails + 1))
-	fi
+	flipCoins
 done
-
 
 echo "Heads got $totalHeads : Tails got $totalTails"
 
-
 if [ "$totalHeads" -gt "$totalTails" ]; then
 	wonBy=$(( "$totalHeads" - "$totalTails"))
-	echo "Head Won by : $wonBy"
+	echo "Heads Won by : $wonBy"
 
 elif [ "$totalHeads" -lt "$totalTails" ]; then
         wonBy=$(( "$totalTails" - "$totalHeads"))
-        echo "Tail Won by : $wonBy"
+        echo "Tails Won by : $wonBy"
 
 elif [ "$totalHeads" -eq "$totalTails" ]; then
         echo "Oops ! Its Tie "
